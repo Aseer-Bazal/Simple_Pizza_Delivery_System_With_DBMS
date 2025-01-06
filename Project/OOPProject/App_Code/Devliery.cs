779d68edfc7b1188112476e99d70cd47ce0a10ea
Aseer-Bazal <aseer.bazal109@gmail.com>
2025-01-06 20:10:16 +0500

    Add files via upload

---
+++ Project/OOPProject/App_Code/Devliery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public abstract class User
+{
+    public string name { get; set; }
+    public string phone { get; set; }
+    public string address { get; set; }
+
+    // Abstract method to calculate the price
+    public abstract decimal CalculatePrice(int quantity, string pizzaSize);
+    
+}
+public class Delivery : User
+{
+    
+    public string pizzaSize { get; set; }
+    public string toppings { get; set; }
+    public int quantity { get; set; }
+    public decimal totalPrice { get; set; }
+
+    public override decimal CalculatePrice(int quantity, string pizzaSize)
+    {
+        if (pizzaSize == "Small")
+        {
+            return quantity * 1000;
+        }
+        else if (pizzaSize == "Medium")
+        {
+            return quantity * 1500;
+        }
+        else if (pizzaSize == "Large")
+        {
+            return quantity * 2000;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+}
---
+++ Project/OOPProject/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OPPProject.Startup))]
+namespace OPPProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
